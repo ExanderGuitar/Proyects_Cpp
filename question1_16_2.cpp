2393e1af6c7e4907b7375f7bd3c816bfd02d1c9e
Exander <55327537+ExanderGuitar@users.noreply.github.com>
2023-10-24 02:03:50 +0200

    Code files from the quiz section of chapter 16.2

---
+++ question1_16_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector numbers {1, 4, 9, 16, 25};
+
+    return 0;
+}
---
+++ question3_16_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<double> yearTemp (365);
+
+    return 0;
+}
